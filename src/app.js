9e919df26dbf3aad502e7b497c7326516b16dd7f
Gultur <monsieur_yan@hotmail.com>
2018-01-25 19:28:09 +0100

    installation transform-class-properties

--- src/app.js
+++ src/app.js
@@ -3,5 +3,29 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
+class OldSyntax{
+    constructor(){
+        this.name = 'Mike';
+        this.getGreeting = this.getGreeting.bind(this)
+    }
+
+    getGreeting() {
+        return `Hi, My name is $(this.name)`;
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+
+// plugin babel 
+class NewSyntax{
+    name = "jen";
+    getGreeting = () => {
+        return `Hi, My name is $(this.name)`;
+    }
+}
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
